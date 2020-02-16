b4536e8ade9309840103698dfd0eed6f5f87f6cd
Theodore Keloglou <zf@sirodoht.com>
2020-02-16 13:19:09 +0000

    rename project

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# nutclean
+# hasol
--- manage.py
+++ manage.py
@@ -11,7 +11,7 @@ def main():
     dotenv_path = join(dirname(__file__), ".env")
     load_dotenv(dotenv_path)
 
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nutclean.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hasol.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
