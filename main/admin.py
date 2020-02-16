d2404b1ac8de0e398b4bf4530b6468e3b0a46fb8
Theodore Keloglou <zf@sirodoht.com>
2020-02-16 15:01:58 +0000

    add basic models

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from main import models
+
+admin.site.register(models.Mate)
+admin.site.register(models.Job)
+admin.site.register(models.Assignment)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Mate(models.Model):
+    name = models.CharField(max_length=300)
+    joined_at = models.DateTimeField(default=timezone.now)
+
+
+class Job(models.Model):
+    title = models.CharField(max_length=300)
+    created_at = models.DateTimeField(default=timezone.now)
+
+
+class Assignment(models.Model):
+    mate = models.ForeignKey(Mate, null=True, on_delete=models.SET_NULL)
+    job = models.ForeignKey(Job, null=True, on_delete=models.SET_NULL)
