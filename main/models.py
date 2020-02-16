2b6a27fa625306caeb49cf4a888a126dd2754cde
Theodore Keloglou <zf@sirodoht.com>
2020-02-16 15:45:52 +0000

    add str methods on models

--- main/models.py
+++ main/models.py
@@ -6,12 +6,21 @@ class Mate(models.Model):
     name = models.CharField(max_length=300)
     joined_at = models.DateTimeField(default=timezone.now)
 
+    def __str__(self):
+        return self.name
+
 
 class Job(models.Model):
     title = models.CharField(max_length=300)
     created_at = models.DateTimeField(default=timezone.now)
 
+    def __str__(self):
+        return self.title
+
 
 class Assignment(models.Model):
     mate = models.ForeignKey(Mate, null=True, on_delete=models.SET_NULL)
     job = models.ForeignKey(Job, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.mate.name + " - " + self.job.title
