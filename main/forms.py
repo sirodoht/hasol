1082afd7fbf0289f6b2bfaa00808bfb619716ad5
Theodore Keloglou <zf@sirodoht.com>
2020-02-19 21:54:35 +0000

    add unsubscribe forms

--- main/forms.py
+++ main/forms.py
@@ -11,3 +11,18 @@ class NotificationForm(forms.ModelForm):
             "email": "Email",
             "mate": "Who are you?",
         }
+
+
+class UnsubscribeForm(forms.ModelForm):
+    class Meta:
+        model = models.Notification
+        fields = ["email"]
+        labels = {
+            "email": "Email to be deleted from notifications",
+        }
+
+
+class UnsubscribeOneclickForm(forms.ModelForm):
+    class Meta:
+        model = models.Notification
+        fields = ["key"]
