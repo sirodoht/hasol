a28fd53dc6ee44971dc1361c755c886e0a4dcae2
Theodore Keloglou <zf@sirodoht.com>
2020-03-02 00:52:07 +0000

    add detailed models on admin

--- main/admin.py
+++ main/admin.py
@@ -2,7 +2,37 @@ from django.contrib import admin
 
 from main import models
 
+
+class MateAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "joined_at")
+
+
 admin.site.register(models.Mate)
-admin.site.register(models.Job)
-admin.site.register(models.Assignment)
-admin.site.register(models.Notification)
+
+
+class JobAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "created_at")
+
+
+admin.site.register(models.Job, JobAdmin)
+
+
+class AssignmentAdmin(admin.ModelAdmin):
+    list_display = ("id", "mate", "job", "week_start")
+
+
+admin.site.register(models.Assignment, AssignmentAdmin)
+
+
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ("id", "email", "mate", "key")
+
+
+admin.site.register(models.Notification, NotificationAdmin)
+
+
+class NotificationSentAdmin(admin.ModelAdmin):
+    list_display = ("id", "notification", "sent_at")
+
+
+admin.site.register(models.NotificationSent, NotificationSentAdmin)
